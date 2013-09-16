040811fde63a9951451ad152bd40993bcd421dfb
John Barker <jebarker@gmail.com>
2013-09-15 22:37:25 -0400

    Upgrade jayq version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://exampl.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir-cljs "0.3.7"]
-                 [jayq "1.0.0"]
+                 [jayq "2.4.0"]
                  [fetch "0.1.0-alpha2" :exclusions [org.clojure/clojure]]
                  [crate "0.2.3"]
                  [lib-noir "0.6.8"]]
